2910e7844f1c39e8a5ed9633a306ae2c890d6c2f
PMPhuc <phucphan2302@gmail.com>
2023-08-15 14:13:01 +0700

    2nd commit

--- app/js/intern.js
+++ app/js/intern.js
@@ -143,82 +143,166 @@ $(document).ready(function() {
     $(`#${shoe.id}`).replaceWith(shopItem);
 }
 
-  function renderCartItems() {
-    $('#cart').remove();
+  // function renderCartItems() {
+  //   $('#cart').remove();
 
-    var cartCard = $('<div>', { class: classNames.card, id: 'cart' });
-    cartCard.append($('<div>', { class: classNames.cardTop }).append($('<img>', { src: logoPath, class: classNames.cardTopLogo })));
-    cartCard.append($('<div>', { class: classNames.cardTitle }).text('Your cart').append($('<span>', { class: classNames.cardTitleAmount }).text(formatPrice(getTotalCartItemsPrice()))));
-    if (cartItems.length === 0) {
-      cartCard.append($('<div>', { class: classNames.cartCardBody })
-              .append($('<div>', { class: classNames.cartEmpty })
-              .append($('<p>', { class: classNames.cartEmptyText })
-              .text('Your cart is empty.'))));
+  //   var cartCard = $('<div>', { class: classNames.card, id: 'cart' });
+  //   cartCard.append($('<div>', { class: classNames.cardTop }).append($('<img>', { src: logoPath, class: classNames.cardTopLogo })));
+  //   cartCard.append($('<div>', { class: classNames.cardTitle }).text('Your cart').append($('<span>', { class: classNames.cardTitleAmount }).text(formatPrice(getTotalCartItemsPrice()))));
+  //   if (cartItems.length === 0) {
+  //     cartCard.append($('<div>', { class: classNames.cartCardBody })
+  //             .append($('<div>', { class: classNames.cartEmpty })
+  //             .append($('<p>', { class: classNames.cartEmptyText })
+  //             .text('Your cart is empty.'))));
       
-      // cartCardBody.append($('<div>', { class: classNames.cartEmpty }).append($('<p>', { class: classNames.cartEmptyText }).text('Your cart is empty.')));
-    }
-    else {
-      // console.log('cart +1');
-      var cartCardBody = $('<div>', { class: classNames.cartCardBody });
-      // console.log(cartItems);
-      // var cartItemsList = $("<div>").addClass(classNames.cartItems);
+  //     // cartCardBody.append($('<div>', { class: classNames.cartEmpty }).append($('<p>', { class: classNames.cartEmptyText }).text('Your cart is empty.')));
+  //   }
+  //   else {
+  //     // console.log('cart +1');
+  //     var cartCardBody = $('<div>', { class: classNames.cartCardBody });
+  //     // console.log(cartItems);
+  //     // var cartItemsList = $("<div>").addClass(classNames.cartItems);
 
-      for (var i = 0; i < cartItems.length; i++) {
-        var item = cartItems[i];
-        var cartItem = $("<div>").addClass(classNames.cartItem).attr("data-id", item.id);
+  //     for (var i = 0; i < cartItems.length; i++) {
+  //       var item = cartItems[i];
+  //       var cartItem = $("<div>").addClass(classNames.cartItem).attr("data-id", item.id);
 
-        var cartItemLeft = $("<div>").addClass(classNames.cartItemLeft);
-        var cartItemImage = $("<div>")
-          .addClass(classNames.cartItemImage)
-          .css("backgroundColor", item.color)
-          .append($("<div>").addClass(classNames.cartItemImageBlock).append($("<img>").attr("src", item.image)));
+  //       var cartItemLeft = $("<div>").addClass(classNames.cartItemLeft);
+  //       var cartItemImage = $("<div>")
+  //         .addClass(classNames.cartItemImage)
+  //         .css("backgroundColor", item.color)
+  //         .append($("<div>").addClass(classNames.cartItemImageBlock).append($("<img>").attr("src", item.image)));
 
-        cartItemLeft.append(cartItemImage);
+  //       cartItemLeft.append(cartItemImage);
 
-        var cartItemRight = $("<div>").addClass(classNames.cartItemRight);
-        var cartItemName = $("<div>").addClass(classNames.cartItemName).text(item.name);
-        var cartItemPrice = $("<div>").addClass(classNames.cartItemPrice).text(formatPrice(item.price));
+  //       var cartItemRight = $("<div>").addClass(classNames.cartItemRight);
+  //       var cartItemName = $("<div>").addClass(classNames.cartItemName).text(item.name);
+  //       var cartItemPrice = $("<div>").addClass(classNames.cartItemPrice).text(formatPrice(item.price));
 
-        var cartItemActions = $("<div>").addClass(classNames.cartItemActions);
-        var cartItemCount = $("<div>").addClass(classNames.cartItemCount);
+  //       var cartItemActions = $("<div>").addClass(classNames.cartItemActions);
+  //       var cartItemCount = $("<div>").addClass(classNames.cartItemCount);
 
-        var decrementButton = $("<div>")
-          .addClass(classNames.cartItemCountButton)
-          // .addClass(classNames.decrementButton)
-          .attr("data-id", item.id)
-          .attr("data-action", "decrement")
-          .text("-");
-        var cartItemCountNumber = $("<div>").addClass(classNames.cartItemCountNumber).text(item.count);
-        var incrementButton = $("<div>")
-          .addClass(classNames.cartItemCountButton)
-          // .addClass(classNames.incrementButton)
-          .attr("data-id", item.id)
-          .attr("data-action", "increment")
-          .text("+");
+  //       var decrementButton = $("<div>")
+  //         .addClass(classNames.cartItemCountButton)
+  //         // .addClass(classNames.decrementButton)
+  //         .attr("data-id", item.id)
+  //         .attr("data-action", "decrement")
+  //         .text("-");
+  //       var cartItemCountNumber = $("<div>").addClass(classNames.cartItemCountNumber).text(item.count);
+  //       var incrementButton = $("<div>")
+  //         .addClass(classNames.cartItemCountButton)
+  //         // .addClass(classNames.incrementButton)
+  //         .attr("data-id", item.id)
+  //         .attr("data-action", "increment")
+  //         .text("+");
         
-        var cartItemRemove = $("<div>")
-          .addClass(classNames.cartItemRemove)
-          .attr("data-id", item.id)
-          .attr("data-action", "pop")
-          .append($('<img>', { src: removePath}));
-        cartItemCount.append(decrementButton, cartItemCountNumber, incrementButton);
+  //       var cartItemRemove = $("<div>")
+  //         .addClass(classNames.cartItemRemove)
+  //         .attr("data-id", item.id)
+  //         .attr("data-action", "pop")
+  //         .append($('<img>', { src: removePath}));
+  //       cartItemCount.append(decrementButton, cartItemCountNumber, incrementButton);
 
-        var cardTitleAmount = $(`.${classNames.cardTitleAmount}`)
-          .text(formatPrice(getTotalCartItemsPrice()));
+  //       var cardTitleAmount = $(`.${classNames.cardTitleAmount}`)
+  //         .text(formatPrice(getTotalCartItemsPrice()));
 
-        cartItemActions.append(cartItemCount, cartItemRemove);
-        cartItemRight.append(cartItemName, cartItemPrice, cartItemActions);
-        cartItem.append(cartItemLeft, cartItemRight);
+  //       cartItemActions.append(cartItemCount, cartItemRemove);
+  //       cartItemRight.append(cartItemName, cartItemPrice, cartItemActions);
+  //       cartItem.append(cartItemLeft, cartItemRight);
 
-        cartCardBody.append(cartItem);
-      }
-      // cartCardBody.append(cartItemsList);
-      cartCard.append(cartCardBody);
-    }
+  //       cartCardBody.append(cartItem);
+  //     }
+  //     // cartCardBody.append(cartItemsList);
+  //     cartCard.append(cartCardBody);
+  //   }
     
-    var mainContent = $(`.${classNames.mainContent}`);
-    mainContent.append(cartCard);
+  //   var mainContent = $(`.${classNames.mainContent}`);
+  //   mainContent.append(cartCard);
+  // }
+
+  function renderCartItems() {
+  $('#cart').remove();
+
+  var cartCard = $('<div>', { class: classNames.card, id: 'cart' });
+  cartCard.append($('<div>', { class: classNames.cardTop }).append($('<img>', { src: logoPath, class: classNames.cardTopLogo })));
+  cartCard.append($('<div>', { class: classNames.cardTitle }).text('Your cart').append($('<span>', { class: classNames.cardTitleAmount }).text(formatPrice(getTotalCartItemsPrice()))));
+
+  if (cartItems.length === 0) {
+    renderEmptyCart(cartCard);
+  } else {
+    renderNonEmptyCart(cartCard);
   }
+
+  var mainContent = $(`.${classNames.mainContent}`);
+  mainContent.append(cartCard);
+}
+
+function renderEmptyCart(cartCard) {
+  var cartCardBody = $('<div>', { class: classNames.cartCardBody });
+  cartCardBody.append(
+    $('<div>', { class: classNames.cartEmpty }).append(
+      $('<p>', { class: classNames.cartEmptyText }).text('Your cart is empty.')
+    )
+  );
+  cartCard.append(cartCardBody);
+}
+
+function renderNonEmptyCart(cartCard) {
+  var cartCardBody = $('<div>', { class: classNames.cartCardBody });
+
+  for (var i = 0; i < cartItems.length; i++) {
+    var item = cartItems[i];
+    var cartItem = $("<div>").addClass(classNames.cartItem).attr("data-id", item.id);
+
+    var cartItemLeft = $("<div>").addClass(classNames.cartItemLeft);
+    var cartItemImage = $("<div>")
+      .addClass(classNames.cartItemImage)
+      .css("backgroundColor", item.color)
+      .append($("<div>").addClass(classNames.cartItemImageBlock).append($("<img>").attr("src", item.image)));
+
+    cartItemLeft.append(cartItemImage);
+
+    var cartItemRight = $("<div>").addClass(classNames.cartItemRight);
+    var cartItemName = $("<div>").addClass(classNames.cartItemName).text(item.name);
+    var cartItemPrice = $("<div>").addClass(classNames.cartItemPrice).text(formatPrice(item.price));
+
+    var cartItemActions = $("<div>").addClass(classNames.cartItemActions);
+    var cartItemCount = $("<div>").addClass(classNames.cartItemCount);
+
+    var decrementButton = $("<div>")
+      .addClass(classNames.cartItemCountButton)
+      // .addClass(classNames.decrementButton)
+      .attr("data-id", item.id)
+      .attr("data-action", "decrement")
+      .text("-");
+    var cartItemCountNumber = $("<div>").addClass(classNames.cartItemCountNumber).text(item.count);
+    var incrementButton = $("<div>")
+      .addClass(classNames.cartItemCountButton)
+      // .addClass(classNames.incrementButton)
+      .attr("data-id", item.id)
+      .attr("data-action", "increment")
+      .text("+");
+    
+    var cartItemRemove = $("<div>")
+      .addClass(classNames.cartItemRemove)
+      .attr("data-id", item.id)
+      .attr("data-action", "pop")
+      .append($('<img>', { src: removePath}));
+    cartItemCount.append(decrementButton, cartItemCountNumber, incrementButton);
+
+    var cardTitleAmount = $(`.${classNames.cardTitleAmount}`)
+      .text(formatPrice(getTotalCartItemsPrice()));
+
+    cartItemActions.append(cartItemCount, cartItemRemove);
+    cartItemRight.append(cartItemName, cartItemPrice, cartItemActions);
+    cartItem.append(cartItemLeft, cartItemRight);
+
+    cartCardBody.append(cartItem);
+  }
+  // cartCardBody.append(cartItemsList);
+  cartCard.append(cartCardBody);
+}
+
 
   function addToCart(item) {
     if (item && !item.inCart) {
